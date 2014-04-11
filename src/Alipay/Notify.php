0d8210c6c17017250c7280a1cca10467bb68bb9e
Merblr <merblr@live.com>
2014-04-11 15:11:18 +0800

    Fix notify

--- src/Alipay/Notify.php
+++ src/Alipay/Notify.php
@@ -4,10 +4,22 @@ use GuzzleHttp\Client as Remote;
 
 class Notify {
 
+    protected $config;
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public static function make($config)
+    {
+        return new static($config);
+    }
+
     public function verify($data)
     {
-        if (Sign::verify($data)) {
-            return Remote::get(notify_url($pid, $notify_id))->getBody == 'true';
+        if (Sign::make($config['key'])->verify($data)) {
+            return Remote::get(notify_url($config['pid'], $notify_id))->getBody == 'true';
         }
         return false;
     }
